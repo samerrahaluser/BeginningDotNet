e67eefc3e8e2070d217e10a9c1d49637e4ec3be0
samerrahhaluser <samer.rahal@treasuryxpress.com>
2020-11-15 18:58:07 +0200

    Added Final Message

--- BeginningDotNet/EntryPoint.cs
+++ BeginningDotNet/EntryPoint.cs
@@ -30,8 +30,9 @@
             }
 
         Console.WriteLine("Thank you for entering your confirmed Salary");
-        //Console.WriteLine("The End");
-        }
+        Console.WriteLine("The End");
+        Console.WriteLine("The Final");
+    }
     
     }
 
